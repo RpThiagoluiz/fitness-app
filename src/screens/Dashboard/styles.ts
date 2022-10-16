53140cf65ffe010f30bf822c122847855a14684a
Rpthiagoluiz <rp.thiagoluiz@gmail.com>
2022-10-16 20:22:37 -0300

    feat: new plains

--- src/screens/Dashboard/styles.ts
+++ src/screens/Dashboard/styles.ts
@@ -1,9 +1,5 @@
 import styled from 'styled-components/native';
 
-export const WrapperButton = styled.View`
-  flex: 1;
-  align-self: center;
-  justify-content: flex-end;
-`;
+export const WrapperButton = styled.View``;
 
 export const Title = styled.Text``;
